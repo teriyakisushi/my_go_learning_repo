8404c4e46ddaadb0fd085b5529bffa526dbca8a6
teriyakisushi <yrahkz@outlook.com>
2024-11-20 20:25:07 +0800

    user renmae and msg handle

--- chat_server/user.go
+++ chat_server/user.go
@@ -28,6 +28,10 @@ func (user *User) ListenMessage() {
 	}
 }
 
+func (user *User) Send2User(msg string) {
+	user.chanUser <- msg
+}
+
 func (user *User) HandleMessage(server *Server) {
 	buf := make([]byte, 4096)
 	for {
@@ -40,6 +44,30 @@ func (user *User) HandleMessage(server *Server) {
 			return
 		}
 		msg := string(buf[:n-1])
-		server.Broadcast(user, msg)
+		// 处理用户消息,不广播
+		if msg == "who" && len(msg) == 3 {
+			user.conn.Write([]byte("online user list:\n"))
+			server.map_lock.RLock() // 上锁
+			for _, u := range server.online_map {
+				onlineMsg := u.userAddress + " Online\n"
+				// 谁查询的就给谁发
+				user.Send2User(onlineMsg)
+			}
+			server.map_lock.RUnlock() // 解锁
+		} else if len(msg) > 7 && msg[:7] == "rename|" {
+			newName := msg[7:]
+			if _, ok := server.online_map[newName]; ok {
+				user.Send2User("User name already exists!")
+			} else {
+				server.map_lock.Lock()
+				delete(server.online_map, user.userAddress)
+				user.userAddress = newName
+				server.online_map[newName] = user
+				server.map_lock.Unlock()
+				user.Send2User("Rename successful!")
+			}
+		} else {
+			server.Broadcast(user, msg)
+		}
 	}
 }
